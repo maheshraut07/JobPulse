82b04547c4bd8c175982d8542e31dc424e8b7293
maheshraut07 <rautmahesh213@gmail.com>
2024-09-23 18:56:35 +0530

    Navbar singup and login page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
-import { Button } from "@/components/ui/button"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from "./components/Home"
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/Login',
+    element:<Login/>
+  },
+  {
+    path:'/Signup',
+    element:<Signup/>
+  }
+])
 
 const App = () => {
   
   return (
-    <div className="textxl">
-      <Button>Click me</Button>
-    </div>
+    <>
+     <RouterProvider router={appRouter}/>
+    </>  
   )
 }
 
