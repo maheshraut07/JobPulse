6a339df8d3a1eace066631294b796dd6670a0191
maheshraut07 <rautmahesh213@gmail.com>
2024-09-30 11:44:29 +0530

    added the custom hooks to fetch the data of all the companies in the UI

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,12 +1,13 @@
-import express from 'express'
-import { getCompany, getCompanyById, registerCompany, updateCompany } from '../controllers/company.controller.js'
-import isAuthenticated from '../middlewares/isAuthenticated.js'
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
+import { singleUpload } from "../middlewares/mutler.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route("/register").post(isAuthenticated, registerCompany)
-router.route("/get").get(isAuthenticated, getCompany)
-router.route("/get/:id").get(isAuthenticated, getCompanyById)
-router.route("/update/:id").put(isAuthenticated,updateCompany)
+router.route("/register").post(isAuthenticated,registerCompany);
+router.route("/get").get(isAuthenticated,getCompany);
+router.route("/get/:id").get(isAuthenticated,getCompanyById);
+router.route("/update/:id").put(isAuthenticated,singleUpload, updateCompany);
 
-export default router
+export default router;
--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route("/post").post(isAuthenticated, postJob);
 router.route("/get").get(isAuthenticated, getAllJobs);
-router.route("/get/:id").get(isAuthenticated, getJobById);
 router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
+router.route("/get/:id").get(isAuthenticated, getJobById);
 
 export default router;
