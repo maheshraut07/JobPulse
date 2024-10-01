e150293444df632417bff30df41981af6f224481
maheshraut07 <rautmahesh213@gmail.com>
2024-10-02 00:01:25 +0530

    added the profile picture to the profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -25,7 +25,7 @@ const Profile = () => {
                 <div className='flex justify-between'>
                     <div className='flex items-center gap-4'>
                         <Avatar className="h-24 w-24">
-                            <AvatarImage src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg" alt="profile" />
+                            <AvatarImage src={user?.profile?.profilePhoto} alt="profile" />
                         </Avatar>
                         <div>
                             <h1 className='font-medium text-xl'>{user?.fullname}</h1>
