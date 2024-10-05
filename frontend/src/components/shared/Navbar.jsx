0aaf6da0583b9f0c5998638deb3917c05c2571b5
maheshraut07 <rautmahesh213@gmail.com>
2024-10-06 00:54:18 +0530

    some routing Links Added

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
                 <div>
-                    <h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Pulse</span></h1>
+                    <Link to="/"><h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Pulse</span></h1></Link>
                 </div>
                 <div className='flex items-center gap-12'>
                     <ul className='flex font-medium items-center gap-5'>
